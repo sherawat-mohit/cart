853d9bcb50d159914b2c2c313ef19a2387b4d890
Mohit Sherawat <107291432+sherawat-mohit@users.noreply.github.com>
2023-04-26 10:44:26 +0530

    Added state to the cartItem component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,38 @@
 import React from 'react'
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price:14999,
+            title:'SmartPhone',
+            qty:1,
+            img:''
+        }
+
+    }
     render(){
+        const{price,title,qty} = this.state;
         return(
             <div className='cart-item'>
                 <div className='left-block'>
                     <img style={styles.image}/>
                 </div>
                 <div className='right-block'>
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:"#777"}}>Rs 999</div>
-                    <div style={{color:"#777"}}>Qty : 1</div>
+                    <div style={{fontSize:25}}>
+                        {title}
+                    </div>
+                    <div style={{color:"#777"}}>
+                        Rs {price}
+                    </div>
+                    <div style={{color:"#777"}}>
+                        Qty : {qty}
+                    </div>
                     <div className='cart-item-actions'>
                         {/* buttons */}
+                        <img alt="increase" className='action-icons' src="https://www.svgrepo.com/show/512678/plus-circle-1427.svg"/>
+                        <img alt="decrease" className='action-icons' src="https://www.svgrepo.com/show/512494/minus-circle-1426.svg"/>
+                        <img alt="delete" className='action-icons' src="https://www.svgrepo.com/show/511178/trash-full.svg"/>
                     </div>
                 </div>
             </div>
